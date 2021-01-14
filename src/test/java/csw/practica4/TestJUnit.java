601994ff135a2d387e9a5fed9bdd3a032f5fe8e5
fandreo2 <sistemas@192.168.1.36>
2021-01-14 17:49:16 +0100

    Add TestJunit class

---
+++ src/test/java/csw/practica4/TestJUnit.java
@@ -0,0 +1,88 @@
+package csw.practica4;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestJUnit {
+	
+	//declaraci�n de variables
+	float base = 2;
+	float altura = 4;
+	float radio = 8;	
+	float valor1 = 2;
+	float valor2 = 4;
+	float valor3 = 8;		
+	float[] arrayEsperado = {4,8,4,(float) 201.06};
+	float[] arrayObtenido = new float[4];
+	
+	//declaraci�n de objetos
+	CalcularArea cuadrado = new CalcularArea(base);
+	CalcularArea rectangulo = new CalcularArea(base,altura);
+	CalcularArea triangulo = new CalcularArea(base,altura);
+	CalcularArea circulo = new CalcularArea(radio);			
+	CalcularArea cuadradoExpected = new CalcularArea(base);
+	
+	@Test
+	public void testAreaCuadrado() {		 
+		
+		System.out.println("areaCuadrado:" + cuadrado.areaCuadrado());		
+		//comprobamos si dos valores son iguales		
+		cuadrado.areaCuadrado();
+		assertEquals(valor2,cuadrado.areaCuadrado(), 0);
+	 }	
+	
+	@Test
+	public void testAreaRectangulo() {
+		System.out.println("areaRectangulo:" + rectangulo.areaRectangulo());		
+		//comprobamos que se cumple la condici�n  
+		assertTrue(rectangulo.areaRectangulo()==valor2); 		
+	 }
+	
+	@Test
+	public void testAreaTriangulo() {
+		System.out.println("areaTriangulo:" + triangulo.areaTriangulo());		
+		 //comprobamos que NO se cumple la condici�n 
+		 assertFalse(triangulo.areaTriangulo()==valor1);		 
+	 }
+	
+	@Test
+	 public void testAreaCirculo() {
+		 System.out.println("areaCirculo:" + String.format("%.2f",circulo.areaCirculo()));		
+		//comprobamos que el valor no es nulo 			
+		 assertNotNull(circulo.areaCirculo());		 
+	 }
+	
+	@Test
+	 public void testAreaCuadrado2() {
+		//comprobamos que el valor es nulo				
+		 assertNull(cuadrado.areaCuadrado());		 
+	 }
+		
+	@Test
+	public void testAreaRectangulo2() {		
+		//comprobamos si dos objetos est�n referenciados al mismo objeto
+		assertSame(cuadradoExpected, cuadrado);		
+	 }
+	
+	@Test
+	public void testAreaTriangulo2() {
+		//comprobamos si dos objetos NO est�n referenciados al mismo objeto		
+		assertNotSame(cuadrado,rectangulo);
+	 }
+	
+	@Test
+	public void testArrays() {
+		
+		arrayObtenido[0]=cuadrado.areaCuadrado();
+		arrayObtenido[1]=rectangulo.areaRectangulo();
+		arrayObtenido[2]=triangulo.areaTriangulo();
+		arrayObtenido[3]=circulo.areaCirculo();	
+		
+		//comprobamos si dos arrays con iguales
+		assertArrayEquals(arrayEsperado, arrayObtenido, 0);
+	 }
+		
+	
+	
+	
+
+}
